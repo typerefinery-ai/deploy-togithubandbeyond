52d6a620bcef6eff19fe3ce92f765ded5e2e1c0c
Max Barrass <max.barrass@gmail.com>
2023-05-23 02:08:23 +1000

    Update fixlinks.py

--- .github/workflows/fixlinks.py
+++ .github/workflows/fixlinks.py
@@ -7,9 +7,15 @@ def update_html_files(folder_path, static_prefix):
         for file_name in files:
             if file_name.endswith(".html"):
                 file_path = os.path.join(root, file_name)
-                update_html_file(file_path, static_prefix)
+                updated_links = update_html_file(file_path, static_prefix)
+                print(f"File: {file_path}")
+                print("Updated Links:")
+                print("\n".join(updated_links))
+                print()
 
 def update_html_file(file_path, static_prefix):
+    updated_links = []
+
     with open(file_path, "r+") as file:
         content = file.read()
         soup = BeautifulSoup(content, "html.parser")
@@ -20,22 +26,30 @@ def update_html_file(file_path, static_prefix):
             for element in elements:
                 href = element["href"]
                 if href.startswith("/assets") or href.startswith("/etc"):
-                    element["href"] = static_prefix + href
+                    updated_href = static_prefix + href
+                    element["href"] = updated_href
+                    updated_links.append(f"Original: {href}\nUpdated: {updated_href}\n")
 
         css_elements = soup.find_all("link", rel="stylesheet", href=True)
         for css_element in css_elements:
             href = css_element["href"]
             if href.startswith("/assets") or href.startswith("/etc"):
-                css_element["href"] = static_prefix + href
+                updated_href = static_prefix + href
+                css_element["href"] = updated_href
+                updated_links.append(f"Original: {href}\nUpdated: {updated_href}\n")
 
         font_elements = soup.find_all("style")
         for font_element in font_elements:
             if "@font-face" in font_element.string:
-                font_element.string = font_element.string.replace("/assets", static_prefix)
+                updated_string = font_element.string.replace("/assets", static_prefix)
+                font_element.string = updated_string
+                updated_links.append("Updated font URLs\n")
 
         file.seek(0)
         file.write(str(soup))
         file.truncate()
+
+    return updated_links
 
 # Usage example
 if __name__ == "__main__":
